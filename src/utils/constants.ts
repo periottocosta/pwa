3bf35f22fe54d4719c5e0f6aab0c3996c1efea6c
periottocosta <gabrielperiotto@hotmail.com>
2024-10-03 22:18:23 -0300

    feat: create api client

---
+++ src/utils/constants.ts
@@ -0,0 +1,27 @@
+
+export const WEEK_DAYS = [
+    'Sun',
+    'Mon',
+    'Tue',
+    'Wed',
+    'Thu',
+    'Fri',
+    'Sat',
+];
+
+export const API = {
+    BASE_URL: "https://openholidaysapi.org",
+    HOLIDAYS: "/publicHolidays",
+    SCHOOL_HOLIDAYS: "/school-holidays",
+    COUNTRY: "/countries",
+    SUB_DIVISION: "/subdivisions",
+}
+
+export const STORAGE_KEYS = {
+    COUNTRY: "COUNTRY",
+    USER_SETTINGS: "USER_SETTINGS",
+    ALL_COUNTRIES: "ALL_COUNTRIES",
+    SUB_DIVISION: "SUB_DIVISION",
+    HOLIDAYS: "HOLIDAYS",
+    ALL_SUB_DIVISIONS: "ALL_SUB_DIVISIONS",
+}
